c218973828474b9ff1155f85116e384e7673012d
hilfear <niemad-vip@hotmail.com>
2014-08-19 22:14:52 +0200

    Merge branch 'master' of C:\Users\Dams\WebStormProject\TFEClient with conflicts.

---
+++ app/js/src/controllers/InsulinDosageCtrl.js
@@ -0,0 +1,35 @@
+var InsulinDosageCtrl = module.exports = function ($scope, InsulinDosageService) {
+    this.$scope = $scope;
+    this.InsulinDosageService = InsulinDosageService;
+    $scope.date = new Date();
+
+    $scope.hstep = 1;
+    $scope.mstep = 15;
+
+    $scope.options = {
+        hstep: [1, 2, 3],
+        mstep: [1, 5, 10, 15, 25, 30]
+    };
+
+    $scope.ismeridian = false;
+    $scope.toggleMode = function() {
+        $scope.ismeridian = ! $scope.ismeridian;
+    };
+
+    $scope.changed = function () {
+        console.log('Time changed to: ' + $scope.date);
+    };
+
+    $scope.clear = function() {
+        $scope.mytime = null;
+    };
+
+};
+
+InsulinDosageCtrl.prototype.ajouterInsulinDosage = function() {
+    this.InsulinDosageService.addInsulinDosage(this.$scope.selectedBoarders[0].id, this.$scope.date.getMilliseconds(), this.$scope.dosage).then(function () {});
+};
+
+InsulinDosageCtrl.prototype.getInsulinDosages = function() {
+    this.InsulinDosageService.getInsulinDosages().then(function () {});
+};
---
+++ app/js/src/services/InsulinDosageService.js
@@ -0,0 +1,35 @@
+var InsulinDosageService = module.exports = function (SERVER_ROOT, $http, $q) {
+    this.$http = $http;
+    this.SERVER_ROOT = SERVER_ROOT;
+    this.$q = $q;
+};
+
+InsulinDosageService.prototype.addInsulinDosage = function (idBoarder, date, dosage) {
+    var _this = this;
+    var data = "boarder=" + idBoarder +
+        "&date=" + date +
+        "&dosage=" + dosage;
+
+    return this.$http.post(this.SERVER_ROOT + 'insulinDosage', data, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic c2FtOnNhbQ=='
+        }
+    }).then(function (data) {
+
+    }).catch(function (data) {
+
+    });
+};
+
+InsulinDosageService.prototype.getInsulinDosages = function () {
+    var _this = this;
+    return this.$http.get(this.SERVER_ROOT + 'insulinDosage/all', {
+        headers: {
+            'Authorization': 'Basic c2FtOnNhbQ=='
+        }}).then(function (response) {
+        return response.data;
+    }).catch(function (response) {
+
+    });
+};
